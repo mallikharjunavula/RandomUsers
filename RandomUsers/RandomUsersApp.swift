63d196b7949e030a419b567cc71fce4556255fa7
arjun <mallikharjunareddyavula@gmail.com>
2023-02-13 10:27:47 +0530

    First Commit: Initialise the project

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Mallikharjuna avula on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
