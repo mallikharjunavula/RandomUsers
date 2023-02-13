8b2056dd749f04ee2c4c9e48d42fc4eed5fb9541
arjun <mallikharjunareddyavula@gmail.com>
2023-02-13 10:58:46 +0530

    Add a Searchbar with back button to the top of the List

--- RandomUsers/View/UsersListView.swift
+++ RandomUsers/View/UsersListView.swift
@@ -18,6 +18,14 @@ struct UsersListView<Model>: View where Model: RandomUsersViewModelProtocol {
     var body: some View {
         Text("HelloWorld")
         NavigationView {
+                    CustomSearchBar()
+                        .frame(height: 35.0)
+                        .background(RoundedRectangle(cornerRadius: 20).fill(Color.mediumGrey))
+                        .foregroundColor(.black)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20.0)
+                                .stroke(Color.darkGrey, lineWidth: 2) )
+                        .padding([.leading, .trailing], 8.0)
                 // List of the RandomUsers
                 List {
                     ForEach(resultsData) { user in
@@ -38,6 +46,32 @@ struct UsersListView<Model>: View where Model: RandomUsersViewModelProtocol {
         }
     }
     
+    //HStack for the Searchbar and back button inside searchbar
+    func CustomSearchBar() -> some View {
+        return HStack {
+            TextField("Search", text: $searchText, onCommit: {
+                isEditing = false
+            })
+                .onChange(of: searchText) { _ in
+                    updateFilters()
+                }
+                .onTapGesture {
+                    isEditing = true
+                }
+                .offset(x: 10.0)
+            // Add the delete button along with SearchBar
+            if isEditing {
+                Button(action: {
+                        searchText = ""
+                    },
+                     label: {
+                        Image(systemName: "delete.left")
+                            .foregroundColor(Color(UIColor.opaqueSeparator))
+                    })
+                    .padding(.trailing, 8.0)
+                }
+            }
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
