9cceca95b26af84e039d7e7a88644d77ed98744d
arjun <mallikharjunareddyavula@gmail.com>
2023-02-13 10:47:25 +0530

    Define the required variables for storing and fetching the users data

--- RandomUsers/Models/RandomViewModel.swift
+++ RandomUsers/Models/RandomViewModel.swift
@@ -9,7 +9,21 @@ import Foundation
 import Combine
 
 protocol RandomUsersViewModelProtocol: ObservableObject {
+    var randomUsers: [User] { get set }
+    var imageList: [String: Data?] { get set }
+    func fetchRandomUsers()
+    func fetchImage(imageURL: String)
+    init(apiFetcher: DataFetcher)
 }
 
 class RandomUsersViewModel {
+    @Published var randomUsers: [User]
+    private var dataFetcher: DataFetcher
+    private var disposables = Set<AnyCancellable>()
+    @Published var imageList = [String : Data?]()
+    
+    required init(apiFetcher: DataFetcher) {
+        dataFetcher = apiFetcher
+        randomUsers = []
+    }
 }
