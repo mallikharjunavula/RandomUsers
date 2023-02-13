fdd17f414f62c743a1521150c8c0b3943b8c2784
arjun <mallikharjunareddyavula@gmail.com>
2023-02-13 10:37:00 +0530

    Create the Model and the ViewModel

---
+++ RandomUsers/Models/RandomViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  RandomViewModel.swift
+//  RandomUsersList
+//
+//  Created by Mallikharjuna avula on 10/02/23.
+//
+
+import Foundation
+import Combine
+
+protocol RandomUsersViewModelProtocol: ObservableObject {
+}
+
+class RandomUsersViewModel {
+}
